dfe68088aca7a0ff21a2f9d6ae23061730bb1f93
ayomide <adebayoayomide396@gmail.com>
2022-08-29 22:54:31 +0100

    loader commit

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 const quotesGenerator = document.getElementById("qoutes_gen");
  
-const adviceID = document.getElementById("advice_id");
-const adviceText = document.getElementById("advice_text");
-const advice = document.querySelector("advice");
+const adviceID = document.querySelector(".advice_id");
+const adviceText = document.querySelector(".advice_text");
+const advice = document.querySelector(".advice");
+const div = document.querySelector("#text");
 
  
- 
+
 const renderAdvice = (data) => {
     adviceID.innerText = `Advice ID: ${data.slip.id}`;
     adviceText.innerText = `"${data.slip.advice}"`
 };
+const renderSpinner = () => {
+    const html = `
+<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+`;
+    div.innerHTML = html  
+};
+ 
+const TIMEOUT_SEC = 2;
+function disableButton() {
+    quotesGenerator.disabled = true;
+    setTimeout(function () { quotesGenerator.disabled = false; },  3000);
+}
 
-
-quotesGenerator.addEventListener('click',    
-    fetchAdvice = async () => {    
+const fetchAdvice = async () => {
     const res = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
-    quotesGenerator.addEventListener('click', renderAdvice(data));  
+    console.log(res);
+    if (res.ok === true) {
+        quotesGenerator.addEventListener('click', renderAdvice(data))
+        quotesGenerator.addEventListener('click', renderSpinner());
+         
+        disableButton();
+        div.innerHTML = '';  
+    }  
+};
 
-});             
+
+quotesGenerator.addEventListener('click', function () {
+    renderSpinner(); 
+    fetchAdvice();
+
+});
+ 
  
